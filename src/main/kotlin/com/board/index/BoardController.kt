8a9f5ad6ed4ca88a0f8176a1d0079f1749a010b6
indextrown <indextrown@gmail.com>
2025-11-10 00:21:09 +0900

    feat: read 구현

--- src/main/kotlin/com/board/index/BoardController.kt
+++ src/main/kotlin/com/board/index/BoardController.kt
@@ -1,10 +1,14 @@
 package com.board.index
 
 import com.board.util.Logger
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/board")
@@ -18,5 +22,20 @@ class BoardController(
         Logger.d("디버그 로그 테스트")
         Logger.w("경고 로그 테스트")
         Logger.e("에러 로그 테스트")
+    }
+
+    @GetMapping
+    fun getAll(): List<BoardDto> {
+        return boardService.getAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): BoardDto {
+        return boardService.getById(id)
+    }
+
+    @GetMapping("/{id}/nullable")
+    fun getByIdOrNull(@PathVariable id: Long): BoardDto {
+        return boardService.getByIdOrNull(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.")
     }
 }
--- src/main/kotlin/com/board/index/BoardService.kt
+++ src/main/kotlin/com/board/index/BoardService.kt
@@ -1,5 +1,6 @@
 package com.board.index
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,10 +16,48 @@ class BoardService (
         )
         boardRepository.save(boardEntity)
     }
+
+    // 전체 조회
+    fun getAll(): List<BoardDto> {
+        val boardEntities = boardRepository.findAll()
+        val boardDtoList = boardEntities.map { board -> // 이부분 없애고 it으로 접근가능
+            BoardDto(
+                id = board.id,
+                title = board.title,
+                content = board.content,
+                name = board.name
+            )
+        }
+        return boardDtoList
+    }
+
+    // 단건 조회
+    fun getById(id: Long): BoardDto {
+        val boardEntity = boardRepository.findById(id).get()
+        val boardDto = BoardDto(
+            id = boardEntity.id,
+            title = boardEntity.title,
+            content = boardEntity.content,
+            name = boardEntity.name
+        )
+        return boardDto
+    }
+
+    fun getByIdOrNull(id: Long): BoardDto? {
+        val boardEntity = boardRepository.findByIdOrNull(id) ?: return null // 존재하지 않으면 null 반환
+        val boardDto = BoardDto(
+            id = boardEntity.id,
+            title = boardEntity.title,
+            content = boardEntity.content,
+            name = boardEntity.name
+        )
+        return boardDto
+    }
 }
 
 /*
-에티티로 처음부터 받지 않고 Dto로 받는 이유
+엔티티로 처음부터 받지 않고 Dto로 받는 이유
 - 엔티티는 영속성 컨텍스트 개념 존재
 - 엔티티 값을 변경하거나 수정시 데이터를 직접 건들 수 있어서 오류 유발 방지
+- 엔티티를 직접 컨트롤러에 넘겨서 프론트와 상호작용하면 안된다 -> 레포지토리에서 조회된 데이터는 엔티티로 반환되는데 이를 DTO로 바꾸어 줘야 한다
  */
